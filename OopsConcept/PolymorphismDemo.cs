eb7deb44bee1bd72a2b81ceeafa850e445092f5e
Ishwar Rathod <ishwarkrathod.7@gmail.com>
2022-12-22 09:54:46 +0530

    polymorphism

---
+++ OopsConcept/PolymorphismDemo.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OopsConcept
+{
+    public class PolymorphismDemo
+    {
+        public virtual void add()
+        {
+            int c = 10+10;
+            Console.WriteLine("this is parent class ");
+        }
+        public void add(int a)
+        {
+            int b = a;
+        }
+        public void add(string s)
+        {
+            string str = s;
+        }
+        public void add(int a, string s) //overloading
+        {
+            int b = a;
+            string str = s;
+
+        }
+
+
+    }
+    public class demo1 : PolymorphismDemo
+    {
+        public override void add()
+        {
+            Console.WriteLine("Overriding done in child class");
+        }
+    }
+
+}
--- OopsConcept/Program.cs
+++ OopsConcept/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace OopsConcept
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //InheritanceDemo obj1 = new InheritanceDemo();
+            //obj1.add();
+            //child aob2 = new child();
+            //aob2.sub();
+
+            PolymorphismDemo pobj1 = new PolymorphismDemo();
+            pobj1.add(8);
+            pobj1.add("ishwar");
+            pobj1.add(8, "demo");
+
+            demo1 dobj = new demo1();
+            dobj.add();
+
+           
+        }
+    }
+}
