6bd8c43f569689dd9e9fc843ba5d7cf2dc1a8361
Ishwar Rathod <ishwarkrathod.7@gmail.com>
2022-12-22 09:45:34 +0530

    Inheritance

---
+++ OopsConcept/InheritanceDemo.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OopsConcept
+{
+    public class InheritanceDemo
+    {
+        public void add()
+        {
+            int a = 10, b = 20;
+            int c = a + b;
+            Console.WriteLine("this is a base class: {0}", c);
+        }
+    }
+    public class child : InheritanceDemo
+    {
+        public void sub()
+        {
+            int a = 10, b = 21;
+            int c = b - a;
+            Console.WriteLine("This is child class: {0} ", c);
+        }
+    }
+}
+
+
