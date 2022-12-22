8c887dd0664fa89b8f929645f3f836d580d1e250
Ishwar Rathod <ishwarkrathod.7@gmail.com>
2022-12-22 10:12:52 +0530

    Encaposulation Demo

---
+++ OopsConcept/EncapsulationDemo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OopsConcept
+{
+    public class EncapsulationDemo
+    {
+        private int balance;
+        public int accountNumber
+        {
+            get { return balance; }
+            set { balance = value; }
+
+        }
+
+        public void Display()
+        {
+            Console.WriteLine(this.balance);
+        }
+
+        
+    }
+}
--- OopsConcept/Program.cs
+++ OopsConcept/Program.cs
@@ -9,17 +9,22 @@
             //child aob2 = new child();
             //aob2.sub();
 
-            PolymorphismDemo pobj1 = new PolymorphismDemo();
-            pobj1.add(8);
-            pobj1.add("ishwar");
-            pobj1.add(8, "demo");
+            //PolymorphismDemo pobj1 = new PolymorphismDemo();
+            //pobj1.add(8);
+            //pobj1.add("ishwar");
+            //pobj1.add(8, "demo");
 
-            demo1 dobj = new demo1();
-            dobj.add();
+            //demo1 dobj = new demo1();
+            //dobj.add();
 
-            Operation op1 = new Operation();
-            op1.AddNum();
+            //Operation op1 = new Operation();
+            //op1.AddNum();
 
+            EncapsulationDemo eobj = new EncapsulationDemo();
+            eobj.accountNumber = 10;
+            //Console.WriteLine(eobj.accountNumber);
+
+            eobj.Display();
         }
     }
 }
